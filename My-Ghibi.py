faea376122a7bab43003700f3798031353ddab08
Kanokpol Ninpet <s7070001@kmitl.ac.th>
2014-11-18 23:45:22 +0700

    Create My-Ghibi File

---
+++ My-Ghibi.py
@@ -0,0 +1,11 @@
+'''
+PSIT Project: My Ghibi
+Kanokpol Ninpet & Pisit Triangkul
+'''
+from Tkinter import *
+root = Tk()
+
+message = Label(root,text="Hello,world!")
+message.pack()
+
+root.mainloop()
