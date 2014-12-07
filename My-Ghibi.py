c1ee8c296a3f1e0867692bf2a5d2f194cb725ae9
beatenter <s7070082@it.kmitl.ac.th>
2014-12-07 17:17:58 +0700

    Rebuilt My-Ghibi v0.01
    Change design

--- My-Ghibi.py
+++ My-Ghibi.py
@@ -5,84 +5,42 @@ Kanokpol Ninpet & Pisit Triangkul
 from Tkinter import *
 import tkMessageBox
 import tkSimpleDialog
+import ImageTk
 
 class App:
     def __init__(self, master):
         '''Initial point'''
-        
-        frame1 = Frame(master)
-        frame1.pack(side=RIGHT)
-        self.status = 0
-
+        def hello():
+            print "hello!"
         frame2 = Frame(master)
-        frame2.pack(side=LEFT)
-        self.status = 0
+        frame2.pack(side=RIGHT)
+        status = 0
 
-        self.create_canvas = Button(
-            frame2, text="CreateCanvas", command=self.cre_canvas
+        frame1 = Frame(master)
+        frame1.pack(side=LEFT)
+        status = 0
+        menubar = Menu(root)
+
+        menu = Menu(root, tearoff=0)
+        filemenu = Menu(menubar, tearoff=0)
+        filemenu.add_command(label="New", command=hello)
+        filemenu.add_command(label="Open", command=hello)
+        filemenu.add_command(label="Save", command=hello)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=root.quit)
+        menubar.add_cascade(label="File", menu=filemenu)
+        root.config(menu=menubar)
+
+        canvas = Canvas(
+            frame2, bg="white", width=500, height=500
             )
-        self.create_canvas.pack()
-        
-        self.question = Button(
-            frame2, text="question", relief=RAISED, bitmap="question"\
-            , command=self._help_)
-        self.question.pack()
-
-        self.left_sign = Button(frame2, text="<<", command=self.go_left)
-        self.left_sign.pack(side=LEFT)
-
-        self.mode_sel = Button(frame2, text="MODE", command=self.ch_mode)
-        self.mode_sel.pack(side=LEFT)
-        
-        self.right_sign = Button(frame2, text=">>", command=self.go_right)
-        self.right_sign.pack(side=LEFT)
-
-    def cre_canvas(self):
-        dialog = MyDialog(root)
-        canvas = Canvas(frame1, width=width.apply(), heigth=heigth.apply())
         canvas.pack()
-    def go_left(self):
-        '''To previous selection'''
-        print "Previous. . ."
-    def go_right(self):
-        '''To next selection'''
-        print "Next. . ."
-    def ch_mode(self):
-        '''Change the mode'''
-        if self.status == 0:
-            tkMessageBox.showinfo("Mode Selection", "Top mode")
-            self.status = 1
-        elif self.status == 1:
-            tkMessageBox.showinfo("Mode Selection", "Bottom mode")
-            self.status = 2
-        elif self.status == 2:
-            tkMessageBox.showinfo("Mode Selection", "Accessory mode")
-            self.status = 3
-        elif self.status == 3:
-            tkMessageBox.showinfo("Mode Selection", "Hair mode")
-            self.status = 0
-    def _help_(self):
-        tkMessageBox.showinfo("Tutorial", "Select Mode and click \"<<\" or \">>\" to customize your avatar.")
 
-class MyDialog(tkSimpleDialog.Dialog):
-
-    def body(self, master):
-
-        Label(master, text="Width:").grid(row=0)
-        Label(master, text="Heigth:").grid(row=1)
-
-        self.e1 = Entry(master)
-        self.e2 = Entry(master)
-
-        self.e1.grid(row=0, column=1)
-        self.e2.grid(row=1, column=1)
-        return self.e1
-
-    def apply(self):
-        width = int(self.e1.get())
-        heigth = int(self.e2.get())
-        return width, heigth
-
+        self.img = ImageTk.PhotoImage(file="image\Ok-icon.png")
+        img1 = canvas.create_image(250, 150, image=self.img)
+        img2 = canvas.create_image(250, 200, image=self.img)
+        canvas.delete(img1)
 root = Tk()
+root.title("My Ghibi")
 app = App(root)
 root.mainloop()
