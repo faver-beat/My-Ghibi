3076f56ba8e5287cba16512107f268ebdfd61f9b
Kanokpol Ninpet <s7070001@kmitl.ac.th>
2014-12-07 18:52:55 +0700

    Update My-Ghibi v0.03.3
    insert toolbox

--- My-Ghibi.py
+++ My-Ghibi.py
@@ -39,7 +39,31 @@ class App:
         self.img = ImageTk.PhotoImage(file="image\Ok-icon.png")
         img1 = canvas.create_image(250, 150, image=self.img)
         img2 = canvas.create_image(250, 200, image=self.img)
-        canvas.delete(img1)
+
+        canvas = Canvas(
+            frame1, bg="white", width=150, height=500
+            )
+        canvas.pack()
+        #Head menu#
+        self.head_mn = ImageTk.PhotoImage(file="image\head_mn.png")
+        self.rgt = ImageTk.PhotoImage(file="image\sign_right.png")
+        self.lft = ImageTk.PhotoImage(file="image\sign_left.png")
+        head_mn = canvas.create_image(75, 135, image=self.head_mn)
+        head_rgt = canvas.create_image(75, 135, image=self.rgt)
+        head_lft = canvas.create_image(75, 135, image=self.lft)
+        
+        #Hair Menu#
+        self.hair_mn = ImageTk.PhotoImage(file="image\hair_mn.png")
+        head_mn = canvas.create_image(75, 250, image=self.hair_mn)
+        head_rgt = canvas.create_image(75, 250, image=self.rgt)
+        head_lft = canvas.create_image(75, 250, image=self.lft)
+
+        #Body Menu#
+        self.body_mn = ImageTk.PhotoImage(file="image\\body_mn.png")
+        head_mn = canvas.create_image(75, 355, image=self.body_mn)
+        head_rgt = canvas.create_image(75, 355, image=self.rgt)
+        head_lft = canvas.create_image(75, 355, image=self.lft)
+        
 root = Tk()
 root.title("My Ghibi")
 app = App(root)
