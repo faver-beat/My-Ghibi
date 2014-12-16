eee37383eda51ae6296839a55a087431808c7127
Kanokpol Ninpet <s7070001@kmitl.ac.th>
2014-12-16 16:17:20 +0700

    Update My-Ghibi V0.04(success)
    Finished Random button

--- My-Ghibi.py
+++ My-Ghibi.py
@@ -4,7 +4,6 @@ Kanokpol Ninpet & Pisit Triangkul
 '''
 from Tkinter import *
 import ImageTk
-import random
 class App:
     def __init__(self, master):
         '''Initial point'''
@@ -17,29 +16,41 @@ class App:
             
         def body_changer(side):
             self.num_bd += side
-            if self.num_bd == 0:
+            if self.num_bd <= 0:
                 self.num_bd = 5
-            elif self.num_bd == 6:
+            elif self.num_bd >= 6:
                 self.num_bd = 1
             self.bd = ImageTk.PhotoImage(file="image/body/"+str(self.num_bd)+".png")
             self.canvas.itemconfig(self.s_bd, image=self.bd)
             
         def hair_changer(side):
             self.num_hr += side
-            if self.num_hr == 0:
-                self.num_hr = 5
-            elif self.num_hr == 6:
+            if self.num_hr <= 0:
+                self.num_hr = 8
+            elif self.num_hr >= 9:
                 self.num_hr = 1
             self.hr = ImageTk.PhotoImage(file="image/hair/"+str(self.num_hr)+".png")
             self.canvas.itemconfig(self.s_hr, image=self.hr)
             
         def head_changer(side):
             self.num_hd += side
-            if self.num_hd == 0:
+            if self.num_hd <= 0:
                 self.num_hd = 5
-            elif self.num_hd == 6:
+            elif self.num_hd >= 6:
                 self.num_hd = 1
             self.hd = ImageTk.PhotoImage(file="image/head/"+str(self.num_hd)+".png")
+            self.canvas.itemconfig(self.s_hd, image=self.hd)
+
+        def do_random():
+            import random
+            num_hair = [i+1 for i in xrange(8)]
+            num_head = [i+1 for i in xrange(5)]
+            num_body = [i+1 for i in xrange(5)]
+            self.bd = ImageTk.PhotoImage(file="image/body/"+str(random.choice(num_body))+".png")
+            self.hr = ImageTk.PhotoImage(file="image/hair/"+str(random.choice(num_hair))+".png")
+            self.hd = ImageTk.PhotoImage(file="image/head/"+str(random.choice(num_head))+".png")
+            self.canvas.itemconfig(self.s_bd, image=self.bd)
+            self.canvas.itemconfig(self.s_hr, image=self.hr)
             self.canvas.itemconfig(self.s_hd, image=self.hd)
             
         frame2 = Frame(master)
@@ -72,7 +83,7 @@ class App:
 
         #Random
         self.rdm = ImageTk.PhotoImage(file="image/rdm.png")
-        random = Button(frame1, image=self.rdm)
+        random = Button(frame1, image=self.rdm, command=lambda: do_random())
         random.pack()
 
         #Body Menu
